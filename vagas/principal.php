0cc58aecd9949200cdf52998f6500e5afc2528de
Paulo Sergio <94811102+paulosergio08@users.noreply.github.com>
2023-03-01 23:46:21 -0300

    Add files via upload

---
+++ vagas/principal.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Meu projeto!</title>
+  </head>
+  <body class="bg-dark text-light">
+    <div class="container">
+      <div class="jumbotron bg-danger">
+      <h6>Paulo Sérgio</h6>
+        <h1>Jovem programador </h1>
+        <h6>Vagas para programadores 💻!!</h6>
+
+       
+      </div>
+   
+
+      <div class="form-group">
+        <a href="cadastrar.php">
+      <button type="submit" class="btn btn-success">Cadastrar</button></a>
+    </div>
+    <figure>
+        <img src="https://th.bing.com/th/id/OIP.dkG6fDAkRC0GYx5SP9EkYQHaGF?pid=ImgDet&rs=1" alt="" srcset="" width="32%" height="32%">
+        <img src="https://th.bing.com/th/id/R.cd68fcad128638f3e3118842f1c1137c?rik=DCMpYuwssoIHRA&riu=http%3a%2f%2fwww.jberita.com%2fwp-content%2fuploads%2f2018%2f11%2fCyber-Security-di-Industri-4.0.jpg&ehk=UOUi922%2b%2forjx59QpT1xzgjWkVKJSrQtGkBhl4SBc04%3d&risl=&pid=ImgRaw&r=0" alt="" style="margin-left: 40px;" srcset=""st width="35%" height="38%">
+    
+    </figure>
